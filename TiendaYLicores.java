e7f501a8c463c4e490e2c6160503d9c1275fec72
rikimaza1 <92672626+rikimaza1@users.noreply.github.com>
2022-10-31 16:52:48 +0100

    Add files via upload

---
+++ TiendaYLicores.java
@@ -0,0 +1,28 @@
+package ejercicios;
+
+public class TiendaYLicores {
+
+	public static void main(String[] args) {
+		Licores licores = new Licores();
+		licores.bienvenida();
+	}
+
+	public TiendaYLicores() {
+	}
+
+	public void bienvenida() {
+		System.out.println("!bienvenido a nuestra tienda¡");
+	}
+}
+
+class Licores extends TiendaYLicores {
+	public Licores() {
+		super();
+	}
+
+	@Override
+	public void bienvenida() {
+		super.bienvenida();
+		System.out.println("Si eres menor de edad fuera de aqui!");
+	}
+}
